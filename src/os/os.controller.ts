6013912d2cb450e10373b1072c366a6a7873e907
chenqy <53074364+chenqy-yh@users.noreply.github.com>
2024-03-06 18:55:28 +0800

    update

--- src/os/os.controller.ts
+++ src/os/os.controller.ts
@@ -1,10 +1,28 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import * as fs from 'fs';
+import { OsService } from './os.service';
 
 @Controller('os')
 export class OsController {
+  constructor(private readonly osService: OsService) {}
+
   @Get('grt')
   getServerCurrentRecievedAndTransmit(@Query('path') path: string) {
     return fs.readFileSync(path, 'utf-8');
+  }
+  @Get('info')
+  async getServerInfo() {
+    const disk_t = this.osService.getServerDiskInfo();
+    const cpu_t = this.osService.getServerCputInfo();
+    const load_t = this.osService.getServerLoadInfo();
+    const mem_t = this.osService.getServerMemInfo();
+    const t_list = await Promise.all([cpu_t, load_t, mem_t, disk_t]);
+    return {
+      timestamp: new Date().getTime(),
+      cpu: t_list[0],
+      load: t_list[1],
+      mem: t_list[2],
+      disk: t_list[3],
+    };
   }
 }
--- src/os/os.service.ts
+++ src/os/os.service.ts
@@ -1,4 +1,81 @@
 import { Injectable } from '@nestjs/common';
+import * as osu from 'os-utils';
+import * as di from 'diskinfo';
+import * as si from 'systeminformation';
+import * as fs from 'fs';
+
+// Intel(R) Xeon(R) Platinum 8255C CPU @ 2.50GHz * 1
+// 1个物理CPU，4个物理核心，4个逻辑核心
 
 @Injectable()
-export class OsService {}
+export class OsService {
+  //cpu info
+  async getServerCputInfo() {
+    const t1 = new Promise((res) => {
+      osu.cpuUsage((v) => {
+        res(v);
+      });
+    });
+    const t2: Promise<si.Systeminformation.CpuData> = si.cpu();
+    const query_task = await Promise.all([t1, t2]);
+    const cpu_usage = query_task[0];
+    const cpu_info = query_task[1];
+    return {
+      manufacturer: cpu_info.manufacturer,
+      brand: cpu_info.brand,
+      speed: cpu_info.speed,
+      processors: cpu_info.processors,
+      physical: cpu_info.physicalCores,
+      performance: cpu_info.performanceCores,
+      usage: cpu_usage,
+    };
+  }
+
+  //   最近1/5/15分钟平均负载：
+  // 0.55 / 0.29 / 0.18
+  // 活动进程数 / 总进程数:
+  // 1 / 124
+  // load info
+  async getServerLoadInfo() {
+    const load_info_list = fs
+      .readFileSync('/proc/loadavg', 'utf-8')
+      .split(/\s+/);
+    return {
+      avload_1: load_info_list[0],
+      avload_5: load_info_list[1],
+      avload_15: load_info_list[2],
+      active_total_per: load_info_list[3],
+    };
+  }
+
+  //   可用：252 MB
+  //   已用：2351 MB
+  //   总内存：3693 MB
+  //   available：1051MB
+
+  async getServerMemInfo() {
+    const mem_info: si.Systeminformation.MemData = await si.mem();
+    return {
+      total: mem_info.total,
+      free: mem_info.free,
+      used: mem_info.used,
+      available: mem_info.available,
+    };
+  }
+
+  //    * 挂载点：/
+  // 共：68.78G，可用：45.66G，已用：20.22G
+  // 未分配：48.56G
+  // 文件系统：/dev/vda1
+  // 类型：ext4，系统占用：2.9G
+  async getServerDiskInfo() {
+    return await new Promise((res, rej) => {
+      di.getDrives((err, drives) => {
+        if (err) {
+          rej(err);
+        }
+        res(drives);
+      });
+    });
+  }
+}
